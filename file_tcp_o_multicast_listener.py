c98dc8d0bcc8c109c4c2ae0d004b2b5eae7af470
SimonNass <57091081+SimonNass@users.noreply.github.com>
2024-01-26 18:43:01 +0100

    hold back queue sort by comparator

--- file_tcp_o_multicast_listener.py
+++ file_tcp_o_multicast_listener.py
@@ -1,6 +1,6 @@
 import socket
 import multiprocessing
-import os
+from functools import cmp_to_key
 
 import file_transfer
 import deviceInfo as deviceInfo
@@ -40,9 +40,23 @@ class OrderedMulticastListener(multiprocessing.Process):
                 message = self.r_deliver_queue.get()
                 #add to holdback
                 self.hold_back_queue.append(message)
-                message =  self.check_hold_back_queue()
-                #ordered multicast delivery
-                self.o_deliver_queue.put(message)
+
+                #TODO sort hold back queue
+                self.hold_back_queue = sorted(self.hold_back_queue, key=cmp_to_key(self.compare_vector_clocks))
+                for entry in self.hold_back_queue.copy():#check if we actually can deliver the message or if we need to hold the changes back in the queue a bit longer
+                    filename, vector_clock, temp_filename, sender_ID, message_type, original_sender_id = entry
+                    #holdback check
+                    if not self.vector_clock_condition(vector_clock, original_sender_id):
+                        print("Holding back message in hold back queue")
+                        continue
+                    #remove it from hold back queue
+                    self.hold_back_queue.remove(entry)
+
+                    self.device_info_dynamic.increase_vector_clock_entry(original_sender_id, 1)
+                    self.device_info_dynamic.update_entire_shared_dict(self.shared_dict, self.lock)
+                    #co-deliver message
+                    #ordered multicast delivery
+                    self.o_deliver_queue.put(filename, temp_filename, message_type)
             except KeyboardInterrupt:
                 self.isRunning = False
             # TODO proper exception handling
@@ -62,22 +76,22 @@ class OrderedMulticastListener(multiprocessing.Process):
                 return False
         return True
 
-    def check_hold_back_queue(self) -> (str, str, str):
-        #check if we actually can deliver the message or if we need to hold the changes back in the queue a bit longer
-        filename, vector_clock, temp_filename, sender_ID, message_type, original_sender_id = self.hold_back_queue[0]
-        
-        #holdback check
-        while not self.vector_clock_condition(vector_clock, original_sender_id):
-            print("Holding back message in hold back queue")
-            # TODO do we nead to rotate the queue entrys to afoid deadlocks and starvation ?
-            # TODO add message to hold back queue and ensure other messages are incoming first
-        #remove it from hold back queue
-        filename, vector_clock, temp_filename, sender_ID, message_type, original_sender_id = self.hold_back_queue.pop()
-        #TODO in parallel execution self.hold_back_queue[0] != self.hold_back_queue.pop() possible
-        self.device_info_dynamic.increase_vector_clock_entry(original_sender_id, 1)
-        self.device_info_dynamic.update_entire_shared_dict(self.shared_dict, self.lock)
-        #co-deliver message
-        return filename, temp_filename, message_type
+    def compare_vector_clocks(self, left_message, right_message):
+        l_filename, l_vector_clock, l_temp_filename, l_sender_ID, l_message_type, l_original_sender_id = left_message
+        r_filename, r_vector_clock, r_temp_filename, r_sender_ID, r_message_type, r_original_sender_id = right_message
+
+        difference = 0
+        for key, value in l_vector_clock.items():
+            difference = difference + util.get_or_default(r_vector_clock, key) - value
+
+        if difference < 0:
+            return -1
+        elif difference > 0:
+            return 1
+        else:
+            return -1
+        #return difference / abs(difference)
+
                 
     def update_device_info_dynamic(self):
         self.device_info_dynamic.get_update_from_shared_dict(self.shared_dict)
