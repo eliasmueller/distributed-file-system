fe24a99249c9049b085e452934279013708d61d4
Elias Müller <elias_benedikt.mueller@porsche.de>
2024-01-28 22:36:26 +0100

    feat: delete duplicate tempfiles

--- file_tcp_r_multicast_listener.py
+++ file_tcp_r_multicast_listener.py
@@ -54,7 +54,7 @@ class ReliableMulticastListener(multiprocessing.Process):
     def r_listen(self, message):
         filename, vector_clock, temp_filename, sender_id, message_type, original_sender_id = message
         if self.is_duplicate(message):
-            #TODO discard and delete its tempfile
+            util.delete_file(temp_filename, self.device_info_static.MY_STORAGE)
             print("duplicate")
             return
         self.recieved_messages.append(message)
