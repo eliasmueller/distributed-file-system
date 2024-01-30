c4777cf839cbc7caf3e48bf23e1a4f0ec11a368a
Elias Müller <elias_benedikt.mueller@porsche.de>
2024-01-30 12:12:42 +0100

    feat: try to delay self monitoring until the discovery is done, hoping all files are transferred by then

--- main.py
+++ main.py
@@ -103,11 +103,11 @@ if __name__ == '__main__':
     p_discovery.start()
 
     p_bully = start_bully(device_info_static, device_info_dynamic, shared_queue, shared_device_info_dynamic, lock)
-    p_monitor = start_folder_monitor(device_info_static, device_info_dynamic, shared_queue, shared_device_info_dynamic, lock)
 
     heartbeat = start_heartbeat(device_info_static, shared_device_info_dynamic, lock, interval=5)
 
     p_discovery.join()
+    p_monitor = start_folder_monitor(device_info_static, device_info_dynamic, shared_queue, shared_device_info_dynamic, lock)
 
     # device_info_dynamic = shared_dict.get("device_info_dynamic")
     device_info_dynamic.get_update_from_shared_dict(shared_device_info_dynamic)
